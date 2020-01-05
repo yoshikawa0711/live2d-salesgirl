81be86f279579698c67bf64ac3b05e817542cdb6
yoshikawa0711 <b1017166@fun.ac.jp>
2020-01-05 23:19:00 +0900

    [add]タップでアクションする機能、指を視線で追う機能の追加

---
+++ Assets/組み込みファイル/LookatController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Live2D.Cubism.Framework.LookAt;
+
+public class LookatController : MonoBehaviour, ICubismLookTarget
+{
+    public Vector3 GetPosition()
+    {
+        if (!Input.GetMouseButton(0))
+        {
+            return Vector3.zero;
+        }
+
+        var targetPosition = Input.mousePosition;
+        targetPosition = (Camera.main.ScreenToViewportPoint(targetPosition) * 2) - Vector3.one;
+        Debug.Log("Look at "+ targetPosition);
+        return targetPosition;
+
+        throw new System.NotImplementedException();
+    }
+
+    public bool IsActive()
+    {
+        return true;
+
+        throw new System.NotImplementedException();
+
+    }
+}
---
+++ Assets/組み込みファイル/TouchDetectController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Live2D.Cubism.Framework.Raycasting;
+using Live2D.Cubism.Samples.LookAt;
+
+public class TouchDetectController : MonoBehaviour
+{
+
+    Animator anim;
+    CubismRaycaster raycaster;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        raycaster = GetComponent<CubismRaycaster>();
+
+    }
+
+    void Update()
+    {
+        if (!Input.GetMouseButtonDown(0))
+        {
+            return;
+        }
+
+        var results = new CubismRaycastHit[4];
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        var hitCount = raycaster.Raycast(ray, results);
+
+        for (int i = 0; i < hitCount; i++)
+        {
+            string hitID = results[i].Drawable.name;
+            Debug.Log("クリックしたアートメッシュIDは" + hitID + "です。");
+
+            if (hitID == "HitAreaBody")
+            {
+                anim.SetTrigger("touchBody");
+            }
+
+            if (hitID == "HitAreaHead")
+            {
+                anim.SetTrigger("touchHead");
+            }
+        }
+    }
+}
